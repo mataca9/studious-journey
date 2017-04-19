09de54f0fd3fe88441c000f7705db58a43667acd
mataca9 <mataca9@gmail.com>
2017-04-18 22:04:24 -0300

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -1,10 +1,10 @@
 CREATE TABLE product(
-    id	    number(10) NOT NULL PRIMARY KEY,
-    name	varchar(50) NOT NULL,
-    price   NUMBER(19,4) NOT NULL,
-    brand  varchar(50) NOT NULL,
-    weight  varchar(10) NOT NULL,
-    discount NUMBER(3,2) NOT NULL
+    id          number(10)      NOT NULL PRIMARY KEY,
+    name	    varchar(50)     NOT NULL,
+    price       NUMBER(19,4)    NOT NULL,
+    brand       varchar(50)     NOT NULL,
+    weight      varchar(10)     NOT NULL,
+    discount    NUMBER(3,2)     NOT NULL
 );
 
 INSERT INTO product VALUE (1, 'Whey Protein Concentrado', 76.50, 'Growth', '1kg', 10);
